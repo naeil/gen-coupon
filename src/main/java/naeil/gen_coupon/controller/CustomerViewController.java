931492a6995738cfcbd37654b5f102e24983c3d8
seungjae.oh <os133517@gmail.com>
2026-01-05 14:58:27 +0900

    feat: 화면 작업중

--- src/main/java/naeil/gen_coupon/controller/CustomerViewController.java
+++ src/main/java/naeil/gen_coupon/controller/CustomerViewController.java
@@ -8,17 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
+import naeil.gen_coupon.service.CustomerService;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/customers")
 public class CustomerViewController {
     
+    private final CustomerService customerService;
+
     @GetMapping
     public String users(Model model) {
-        // model.addAttribute("users", orderService.findAll());
+        // model.addAttribute("customers", orderService.findAll());
 
-        model.addAttribute("users", new ArrayList<>());
-        return "users";
+        model.addAttribute("customers", new ArrayList<>());
+        return "customers";
     }
 }
